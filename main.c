ec54173874eab03e7e56f3e7dd92a214b7991a16
TatyanaGeyt <126000211+TatyanaGeyt@users.noreply.github.com>
2023-05-04 11:14:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,50 +7,50 @@ double f1(double x);
 double f2(double x);
 double f3(double x);
 
-int count = 0;
+int count = 0;                                                                         // счётчик количества итерации функции root
 
-double find_x0(double x1, double y1, double x2, double y2){
+double find_x0(double x1, double y1, double x2, double y2){                            // функция для определения абсциссы точки пересечения хорды с осью Ох
     return (x1*(y2 - y1) - y1*(x2 - x1))/(y2 - y1);
 }
-double root(double (*f)(double), double (*g)(double), double a, double b, double eps){
-    double y_a = f(a) - g(a);
-    double y_b = f(b) - g(b);
-    double x = find_x0(a, y_a, b, y_b);
-    double y = f(x) - g(x);
+double root(double (*f)(double), double (*g)(double), double a, double b, double eps){ // F = f-g
+    double y_a = f(a) - g(a);                                                          // значение в точке а
+    double y_b = f(b) - g(b);                                                          // значение в точке b
+    double x = find_x0(a, y_a, b, y_b);                                                // абсцисса точки пересечения хорды с осью Ох
+    double y = f(x) - g(x);                                                            // значение функции F в этой точке
 
-    count++;
+    count++;                                                                           // увеличиваем счётчик количества итераций
 
-    double y_eps1 = f(x + eps) - g(x + eps);
+    double y_eps1 = f(x + eps) - g(x + eps);                                           // значение функции F в окрестновти точки х
     double y_eps2 = f(x - eps) - g(x - eps);
-    if (y * y_eps1 <= 0 || y * y_eps2 <= 0)
-        return x;
-    if (y * y_a >= 0)
+    if (y * y_eps1 <= 0 || y * y_eps2 <= 0)                                            // если значения функции в окрестности и в самой точке совпадают по знаку,
+        return x;                                                                      // то мы нашли искомый корень (с точностью eps) 
+    if (y * y_a >= 0)                                                                  // меняем концы отрезка и запускаем функцию рекурсивно
         return root(f, g, x, b, eps);
     if (y * y_b >= 0)
-        return root(f, g, a, x, eps);
-    return 0.0;
-}
+        return root(f, g, a, x, eps);                                                  // в случае ошибки возвращаем 0 (т.к. мы знаем, что ни одна точка пересечения
+    return -1.0;                                                                        // не имеет абсциссу -1.0)
+
 
 double integral(double (*f)(double), double a, double b, double eps){
-    if (a > b)
-        return integral(f, b, a, eps);
-    int n = 10;
-    double delta = (b - a) / n;
-    double In = -1;
-    double I2n = -2;
-    while((3 * (fabs(In - I2n)) > eps)){
-        In = I2n;
-        double sum = 0;
-        double xi0 = a;
+    if (a > b)                                                                         // мы не хотим перед запуском функции проверять, какой из корней больше
+        return integral(f, b, a, eps);                                                 // поэтому, если a>b, перезапускаем функцию с правильным порядком концов отрезка
+    int n = 10;                                                                        // стартовое значение n
+    double delta = (b - a) / n;                                                        // считаем стартовое значение диаметра разбиения (чтобы потом просто делить его на 2)
+    double In = -1;                                                                    // стартовое значение интегралов (чтобы, во-первых, |In - I2n| был точно больше eps,
+    double I2n = -2;                                                                   // и, во-вторых, чтобы в случае какой-либо ошибки функция вернула неправдоподобное число)
+    while((3 * (fabs(In - I2n)) > eps)){                                               // 3 - множитель, с которым мы с помощью метода (средних) прямоугольников получаем точность eps (т.е. в 3 раза точнее)
+        In = I2n;                                                                      // (согласно Wikipedia)
+        double sum = 0;                                                                
+        double xi0 = a;                                                                // левый конец отрезка
         delta = delta / 2;
         for (int i = 1; i < 2*n; i++){
-            double xi = a + delta*i;
-            double ksii = (xi + xi0) / 2;
-            double y = f(ksii);
-            sum += y;
+            double xi = a + delta*i;                                                   // правый конец текущего отрезка
+            double ksii = (xi + xi0) / 2;                                              // середина текущего отрезка
+            double y = f(ksii);                                                        // значение функции в этой точке
+            sum += y;                                                                  // сумма значений
             xi0 = xi;
         }
-        I2n = delta*sum;
+        I2n = delta*sum;                                                               // значение интеграла
         n = 2 * n;
     }
     return I2n;
@@ -58,16 +58,16 @@ double integral(double (*f)(double), double a, double b, double eps){
 
 int main(int argc, char** argv){
 
-    double (*f[3]) (double) = {&f1, &f2, &f3};
+    double (*f[3]) (double) = {&f1, &f2, &f3};                                         // массив указателей на функции (для -integral)
 
     double eps1 = 0.00001;
     double a = 0.1;
     double b = 3.0;
 
     count = 0;
-    double x1 = root(f1, f2, a, b, eps1);
-    int c1 = count;
-    count = 0;
+    double x1 = root(f1, f2, a, b, eps1);                                              // заранее посчитаем все значения, чтобы потом вывести эти значения (если пользователь попросит)
+    int c1 = count;                                                                    // (пользователь может задействовать несколько ключей одновременно, в этом случае будет эффективно посчитать
+    count = 0;                                                                         // все величины заранее, чем при получении нового ключа снова всё пересчитывать)
     double x2 = root(f2, f3, a, b, eps1);
     int c2 = count;
     count = 0;
@@ -91,9 +91,9 @@ int main(int argc, char** argv){
         }
         else if (strcmp(argv[i], "-root") == 0){
             printf(" roots:\n");
-            printf(" >> f1 and f2: %f\n", x1);
-            printf(" >> f2 and f3 : %f\n", x2);
-            printf(" >> f3 and f1 : %f\n", x3);
+            printf(" >> f1 and f2: %f.3\n", x1);
+            printf(" >> f2 and f3 : %.3f\n", x2);
+            printf(" >> f3 and f1 : %.3f\n", x3);
         }
         else if (strcmp(argv[i], "-rootc") == 0){
             printf(" count:\n");
@@ -105,15 +105,15 @@ int main(int argc, char** argv){
             int findex = argv[i + 1][0] - '0';
             float a1 = strtod(argv[i + 2], 0);
             float b1 = strtod(argv[i + 3], 0);
-            i += 3;
-            if (findex == 3 && a1*b1 = 0){
-                printf(" Error !!!\n");
+            i += 3;                                                                    // сдвигаем счётчик на 3, т.к. приняли ещё 3 дополнительных аргемента
+            if (findex == 3 && a1*b1 <= 0){                                             // если пользователь захочет посчитать интеграл для функции 1/х и возникнет деление на 0
+                printf(" !!! Error in -integral : division by 0\n make sure the segment is correct\n\n");                                                // выведем ошибку и перейдём к следующей итерации цикла
                 continue;
             }
-            printf(" >> area of f%d on segment [ %lf, %lf ] : %.3lf\n", findex, a1, b1, integral(*(f[findex - 1]), a1, b1, eps2));
+            printf(" >> area of f%d on segment [ %.3lf, %.3lf ] : %.3lf\n", findex, a1, b1, integral(*(f[findex - 1]), a1, b1, eps2));
         }
         else if (strcmp(argv[i], "-answer") == 0){
-            printf("\n answer:\n >> %.3lf\n", s);
+            printf(" answer:\n >> %.3lf\n", s);
         }
         
         i++;
