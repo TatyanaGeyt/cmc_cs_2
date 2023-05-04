0d5fa928553b9c6c0fef54778761f11a4c9b9c4a
TatyanaGeyt <126000211+TatyanaGeyt@users.noreply.github.com>
2023-05-04 11:21:54 +0300

    Update func.asm

--- func.asm
+++ func.asm
@@ -14,13 +14,13 @@ f1: ; e^(-x) + 3
     sub esp, 24
     
     finit
-    fld qword[ebp + 8]
-    fld qword[const1]
-    fmulp
+    fld qword[ebp + 8]    ; x
+    fld qword[const1]     
+    fmulp                 ; -x
     fstp qword[esp]
-        call exp
+        call exp          ; e^(-x)
     fld qword[const2]
-    faddp
+    faddp                 ; e^(-x) + 3
     
     leave
     ret
@@ -30,11 +30,11 @@ f2: ; 2x - 2
     mov ebp, esp
     
     finit
-    fld qword[const3]
-    fld qword[ebp + 8]
-    fmulp
-    fld qword[const3]
-    fsubp
+    fld qword[const3]      ; 2
+    fld qword[ebp + 8]     ; x
+    fmulp                  ; 2x
+    fld qword[const3] 
+    fsubp                  ; 2x - 2
     
     leave
     ret
@@ -44,9 +44,9 @@ f3: ; 1/x
     mov ebp, esp
     
     finit
-    fld1
-    fld qword[ebp + 8]
-    fdivp
+    fld1                    ; ноль на вход не подаём, проверяем это в си
+    fld qword[ebp + 8]     
+    fdivp                   ; 1/x
     
     leave
     ret
